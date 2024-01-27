21efb9a6b55d1e64ff2a02b045390a5f0de37153
suraj <suraj.u9903@gmail.com>
2024-01-27 12:17:49 +0530

    commit to master branch

---
+++ src/test/java/testIng_github5/Tester.java
@@ -0,0 +1,11 @@
+package testIng_github5;
+
+public class Tester {
+	
+	public static void tester()
+	{
+		System.out.println("commite to master branch");
+		System.out.println("launch  browser");
+	}
+
+}
