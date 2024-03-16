0424fe59f09e5a713572744f24b5a9a3d2852db3
suraj <suraj.u9903@gmail.com>
2024-03-16 17:39:22 +0530

    Commited

--- src/test/java/testIng_github5/Tester.java
+++ src/test/java/testIng_github5/Tester.java
@@ -4,7 +4,9 @@ public class Tester {
 	
 	public static void tester()
 	{
-		System.setProperty("testing conflict", "");
+		System.setProperty("testing rsovle conflict", "");
+		System.setProperty("testing resolve conflict", "");
+		System.setProperty("testing tester resolved conflict", "");
 	}
 
 }
